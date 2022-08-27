1a9d9fc04b2ce1c543fead0c388cd21b51ad3a27
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-08-27 15:20:20 +0900

    [add] logger

---
+++ pkg/log/logger.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	Logger      *zap.SugaredLogger
+	debugOption bool
+)
+
+func InitLogger(debug bool) (err error) {
+	debugOption = debug
+	Logger, err = newLogger(debug)
+	if err != nil {
+		return fmt.Errorf("error in new logger: %w", err)
+	}
+	return nil
+}
+
+func newLogger(debug bool) (*zap.SugaredLogger, error) {
+	level := zap.NewAtomicLevel()
+	if debug {
+		level.SetLevel(zapcore.DebugLevel)
+	} else {
+		level.SetLevel(zapcore.InfoLevel)
+	}
+
+	zapConfig := zap.Config{
+		Level:       level,
+		Development: debug,
+	}
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build zap config: %w", err)
+	}
+
+	return logger.Sugar(), nil
+}
+
+func Fatal(err error) {
+	if debugOption {
+		Logger.Fatalf("%+v", err)
+	}
+	Logger.Fatal(err)
+}
